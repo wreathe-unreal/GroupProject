2a908c3bdbf982998f4e8c762e8d2d6311a064a8
wreathe-unreal <110839486+wreathe-unreal@users.noreply.github.com>
2023-08-26 13:23:48 +0800

    Ghost AI, Vanishing, and Collision Improvements

--- Assets/Scripts/Ghost.cs
+++ Assets/Scripts/Ghost.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
@@ -5,6 +6,7 @@ using UnityEngine.AI;
 using UnityEngine.XR;
 using static UnityEngine.UIElements.UxmlAttributeDescription;
 using UnityEngine.UIElements;
+using Random = UnityEngine.Random;
 
 public class Ghost : MonoBehaviour
 {
@@ -65,31 +67,32 @@ public class Ghost : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ghostState();
-        ReactToLight();
+        GhostState();
 
     }
 
-    private void ghostState ()
+    private void GhostState ()
     {
         float dist = Vector3.Distance(player.transform.position, transform.position);
 
         bSpotted = ghostFOV.bSpotted;
-
+        
+        ReactToLight();
+        
         //Spotted target
         if (bSpotted || dist < 1.5f)
         {
             ghostAnimate.SetTrigger("spotted");
             ghostGlow.color = Color.red;
-            transform.LookAt(new Vector3(player.transform.position.x, 0, player.transform.position.z));
+            transform.LookAt(new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z));
 
-            agent.destination = new Vector3(player.transform.position.x, 0, player.transform.position.z);
+            agent.destination = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
 
             inPursuit = true;
             
             if (dist < 1.45f)
             {
-                caught();
+                CatchPlayer();
             }
 
         }
@@ -98,9 +101,10 @@ public class Ghost : MonoBehaviour
         {
             //ghostAnimate.SetTrigger("searching");
             ghostGlow.color = Color.yellow;
-            transform.LookAt(new Vector3(player.transform.position.x, 0, player.transform.position.z));
-            agent.destination = new Vector3(player.transform.position.x, 0, player.transform.position.z);
+            transform.LookAt(new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z));
+            agent.destination = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
             pursuitTime += Time.deltaTime;
+            
             if (pursuitTime >= pursuitDur)
             {
                 inPursuit = false;
@@ -114,16 +118,17 @@ public class Ghost : MonoBehaviour
             ghostGlow.color = Color.white;
             if (waypoints.Length > 0)
             {
-                target = new Vector3(waypoints[currentPoint].position.x, 0, waypoints[currentPoint].position.z);
-
+                target = new Vector3(waypoints[currentPoint].position.x, transform.position.y, waypoints[currentPoint].position.z);
+                transform.LookAt(target); 
             }
             
             direction = target - transform.position;
+            direction.y = transform.position.y;
 
             if (direction.magnitude < 1.5 && reachStart == false)
             {
                 reachStart = true;
-                ghostPoints();
+                ChooseBehavior();
                 //currentPoint = Random.Range(0, waypoints.Length);
             }
             
@@ -133,7 +138,7 @@ public class Ghost : MonoBehaviour
         }
     }
 
-    void caught()
+    void CatchPlayer()
     {
         if (!onetime && inPursuit)
         {
@@ -144,51 +149,61 @@ public class Ghost : MonoBehaviour
     }
 
     //Function for random behavior when ghost is at specific waypoint
-    void ghostPoints()
+    void ChooseBehavior()
     {
         int rando = Random.Range(0, 3);
         //int rando = 2;
         switch (rando)
         {
             case 0:         //straight Next poimt
-                Invoke("newPoint", 1.0f);
-                Debug.Log("Go");
+                Invoke("RandomWaypoint", 1.0f);
                 break;
             case 1:         //idle
                 //Invoke("investigate", 1.0f);
-                Invoke("newPoint", 2.0f);
-                Debug.Log("Go2");
+                Invoke("RandomWaypoint", 2.0f);
                 break;
             case 2:         //Looking around
-                ghostAnimate.SetTrigger("looking");
-                Invoke("newPoint", 4.0f);
-                Debug.Log("Go3");
+                LookAnimation();
+                Invoke("RandomWaypoint", 4.0f);
+                Invoke("SearchAnimation", 4.0f);
                 break;
         }
     }
 
-    //Set next waypoint for ghost to patrol
-    void newPoint()
+    void SearchAnimation()
     {
+        ghostAnimate.SetTrigger("searching");
+    }
+    void LookAnimation()
+    {
+        ghostAnimate.SetTrigger("looking");
+    }
+
+    //Set next waypoint for ghost to patrol
+    int RandomWaypoint()
+    {
+        prevPoint = currentPoint; 
         currentPoint = Random.Range(0, waypoints.Length);
         while (currentPoint == prevPoint)
         {
             currentPoint = Random.Range(0, waypoints.Length);
         }
-        prevPoint = currentPoint; 
-        
         reachStart = false;
+        return currentPoint;
     }
 
     void ReactToLight()
     {
-        Vector3 screenPosition = playerCamera.WorldToScreenPoint(transform.position);
+
+        Vector3 screenPosition = playerCamera.WorldToScreenPoint(transform.position + new Vector3(0f, 1.6f, 0f)); //increase the ghost's origin 
         Vector3 screenCenter = new Vector3(Screen.width / 2, Screen.height / 2, 0);
         // Define allowed screen deviation from the center
         float allowedScreenDeviation = 300.0f;
 
 // Compute distance from the screen center to the ghost's projected position
         float distanceFromCenter = Vector3.Distance(screenPosition, screenCenter);
+        Debug.Log(distanceFromCenter);
+        Debug.DrawLine(transform.position + new Vector3(0f, 1.6f, 0f), transform.position + new Vector3(0f, 1.7f, 0f));
         
         if (distanceFromCenter <= allowedScreenDeviation)
         {
@@ -200,10 +215,9 @@ public class Ghost : MonoBehaviour
                 if (player.GetComponent<PlayerCharacter>().bFlashlightActive)
                 {
                     Debug.Log("Player is facing the ghost and not obstructed.");
-                    transform.LookAt((player.transform.position - transform.position).normalized);
                     
                     // Adjust the alpha value
-                    ghostMaterialColor.a -= Time.deltaTime * .4f;
+                    ghostMaterialColor.a -= Time.deltaTime * Math.Max((ghostMaterialColor.a *.99f), 0.3f);
                     if (ghostMaterialColor.a < 0f)
                     {
                         ghostMaterialColor.a = 0f;
@@ -218,24 +232,41 @@ public class Ghost : MonoBehaviour
             {
                 // Obstruction detected
                 Debug.Log("Player is facing the ghost, but there is an obstruction.");
-                ghostMaterialColor.a += Time.deltaTime * .08f;
-                if (ghostMaterialColor.a > 1f)
+                ghostMaterialColor.a += Time.deltaTime * Math.Max((ghostMaterialColor.a *.99f), 0.1f);
+                if (ghostMaterialColor.a > .6f)
                 {
-                    ghostMaterialColor.a = 1f;
+                    ghostMaterialColor.a = .6f;
                 }
                 ghostMaterial.color = ghostMaterialColor;
-                transform.LookAt(new Vector3(player.transform.position.x, 0, player.transform.position.z));
             }
         }
         else
         {
             // Player is not facing the ghost
             Debug.Log("Player is not facing the ghost.");
-            ghostMaterialColor.a = Mathf.Lerp(ghostMaterialColor.a, .6f, Time.deltaTime * 1.0f);
+            ghostMaterialColor.a += Time.deltaTime * Math.Max((ghostMaterialColor.a *.99f), 0.1f);
+            if (ghostMaterialColor.a > .6f)
+            {
+                ghostMaterialColor.a = .6f;
+            }
             ghostMaterial.color = ghostMaterialColor;
-            transform.LookAt(new Vector3(player.transform.position.x, 0, player.transform.position.z));
         }
        
     }
 
+    bool bGhostAlphaAggressive()
+    {
+        if (ghostMaterialColor.a > .4)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    void AlphaBasedCollision()
+    {
+        
+    }
+    
+    
 }
