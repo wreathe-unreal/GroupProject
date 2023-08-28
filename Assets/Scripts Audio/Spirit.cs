6e309ec15da0c1aa87de8480256e23b36ffba203
Luong-Code <zonina123@gmail.com>
2023-08-28 23:29:47 +0800

    Spirit scripts added
    based on each location near to keys, the voice will be activated with their lines, the text UI haven't been assigned for every spirits, only voice.

---
+++ Assets/Scripts Audio/Spirit.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spirit : MonoBehaviour
+{
+    // the script that said something about the dead spirit (principal, etc)
+    public string nameSpirit;
+    public string role;
+
+    // audio part
+    public AudioClip audioClips;
+    public string transcripts;
+
+    private int randomClip;    
+
+    public void PlayDialog()
+    {
+        if (audioClips != null)
+        {
+            AudioClip clip = audioClips;
+            AudioSource.PlayClipAtPoint(clip, transform.position);
+        }
+    }
+
+}
