b7edb7897c7145f40c3c9f0480f70a524d9b367c
Luong-Code <zonina123@gmail.com>
2023-08-29 00:57:54 +0800

    Revert "Merge branch 'main' of https://github.com/wreathe-unreal/GroupProject"
    This reverts commit a330e2cd2d545b78e099822cf49b1304ceb114d8, reversing
    changes made to 007fd2c867dee7bf97d037dfee1bc99383b61e21.

---
+++ Assets/Scripts Audio/VoiceZone.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoiceZone : MonoBehaviour
+{
+    // play one time
+    public bool hasPlayed;
+    // trigger the sound when the player is near to a specific object.
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !hasPlayed)
+        {
+            hasPlayed = true;
+            Spirit spirit = GetComponent<Spirit>();
+            spirit.PlayDialog();
+        }
+    }
+}
